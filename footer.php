e047e00fe702f8056833f8d2c3315183265da563
faridg <faridg@twc.com>
2018-02-11 00:28:20 -0500

    footer from index

---
+++ footer.php
@@ -0,0 +1,18 @@
+<footer class="main-footer">
+	<div class="container">
+		<div class="footer-left">
+			<p>&copy; 2018 - Wordpress Boilerplate Theme</p>
+		</div>
+		<div class="footer-right">
+			<ul>
+				<li><a href="#">Homies</a></li>
+				<li><a href="#">Posts</a></li>
+				<li><a href="#">Pages</a></li>
+			</ul>
+		</div>
+	</div>
+</footer>
+<?php wp_footer(); ?>
+
+</body>
+</html>
